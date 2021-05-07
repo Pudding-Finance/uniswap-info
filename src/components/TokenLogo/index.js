26e52f6262717e17dc87ea07ec4448993ca44480
pudding-dev <pudding.finance@gmail.com>
2021-05-07 22:21:46 +0800

    fix:

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-// import styled from 'styled-components'
-import { isAddress } from '../../utils/index.js'
+import styled from 'styled-components'
+// import { isAddress } from '../../utils/index.js'
 import EthereumLogo from '../../assets/eth.png'
 
 const BAD_IMAGES = {}
