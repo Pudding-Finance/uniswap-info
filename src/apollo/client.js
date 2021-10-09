31ed0905bda47e1b4b5151b7885c3d03aae884cf
pudding-dev <83436748+pudding-dev@users.noreply.github.com>
2021-10-09 22:36:26 +0800

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://hq.hg.network/subgraphs/name/pudding/hsc',
+    uri: 'https://n13.hg.network/subgraphs/name/pudding/hsc',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -20,7 +20,7 @@ export const healthClient = new ApolloClient({
 
 export const v1Client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://hq.hg.network/subgraphs/name/pudding/hsc',
+    uri: 'https://n13.hg.network/subgraphs/name/pudding/hsc',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -28,7 +28,7 @@ export const v1Client = new ApolloClient({
 
 export const stakingClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://hq.hg.network/subgraphs/name/pudding/hsc',
+    uri: 'https://n13.hg.network/subgraphs/name/pudding/hsc',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -36,7 +36,7 @@ export const stakingClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://hq.hg.network/subgraphs/name/hscblocks/hsc',
+    uri: 'https://n13.hg.network/subgraphs/name/hscblocks/hsc',
   }),
   cache: new InMemoryCache(),
 })
